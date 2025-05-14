0f1638122002c1d220ad056cb1edccef73e3dd5e
O-G-W-A-L <ogwal04@gmail.com>
2025-05-14 15:32:37 +0300

    Applied Aggregations and Window Functions

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,15 @@
+-- total number of bookings made by each user, using the COUNT function and GROUP BY clause.
+-- This query retrieves the user_id and the total number of bookings for each user.
+
+SELECT user_id, COUNT(*) AS total_bookings
+FROM booking
+GROUP BY user_id;
+
+--Using a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received
+-- This query retrieves the property_id and the total number of bookings for each property.
+
+SELECT property_id, COUNT(*) AS total_bookings,
+       ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+FROM booking
+GROUP BY property_id;
+
