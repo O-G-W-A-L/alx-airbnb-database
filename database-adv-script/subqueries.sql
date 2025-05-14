13dd2cddd55c095b13aa97b907e65f265d09ba57
O-G-W-A-L <ogwal04@gmail.com>
2025-05-14 15:16:52 +0300

    subqueries

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,8 +1,12 @@
 -- Subqueries
 -- A subquery to find the average rating of properties of properties gretaer than 4.0
 SELECT property_id
-FROM properties
-WHERE AVG(rating) > 4.0
+FROM (
+    SELECT property_id, AVG (rating) AS avg_rating
+    FROM reviews
+    GROUP BY property_id
+) AS avg_ratings
+WHERE avg_rating > 4.0;
 
 --a correlated subquery to find users who have made more than 3 bookings.
 SELECT user_id
