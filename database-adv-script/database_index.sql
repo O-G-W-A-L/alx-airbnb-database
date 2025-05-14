7a88c78aab4876c5831e9496ebc77d4d83f1d6ad
O-G-W-A-L <ogwal04@gmail.com>
2025-05-14 16:54:01 +0300

    Indexes for Optimization

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,4 +1,4 @@
---SQL CREATE INDEX commands to create appropriate indexes
+-- SQL CREATE INDEX commands to create appropriate indexes
 -- on the tables in the database
 
 -- Indexes for Users table
@@ -10,6 +10,6 @@ CREATE INDEX idx_booking_user_id ON booking (user_id);
 CREATE INDEX idx_booking_property_id ON booking (property_id);
 CREATE INDEX idx_booking_booking_date ON booking (booking_date);
 
--- indexes for properties table
-CREATE INDEX idx_properties_host_id ON properties (title);
+-- Indexes for properties table
+CREATE INDEX idx_properties_host_id ON properties (host_id);
 CREATE INDEX idx_properties_location ON properties (location);
